209d3effce12567cdfb5968be2df1cb36c15fa34
DanRunfola <dan@danrunfola.com>
2023-12-15 12:07:28 -0500

    update logging

--- RabbitMQ/worker.py
+++ RabbitMQ/worker.py
@@ -1,37 +1,43 @@
 import pika
 import os
 
-print("Script initialized.")
+def lo(message):
+    with open("/kube/home/test", "a") as f:
+        f.write(message + "\n")
+
+
+
+lo("Script initialized.")
 
 def callback(ch, method, properties, body):
-    print(" [x] Received " + str(body))
-    print("Deleting message with tag: " + str(method.delivery_tag))
+    lo(" [x] Received " + str(body))
+    lo("Deleting message with tag: " + str(method.delivery_tag))
     ch.basic_ack(delivery_tag=method.delivery_tag) #Delete the message from the queue after processing is complete.
 
-print("Retrieving Environmental Variables.")
+lo("Retrieving Environmental Variables.")
 # Replace with your RabbitMQ service name and port.  
 # These are environmental variables defined in the yml.
 rabbitmq_service = os.getenv('RABBITMQ_SERVICE')
 rabbitmq_port = os.getenv('RABBITMQ_PORT')
-print(rabbitmq_service)
-print(rabbitmq_port)
+lo(rabbitmq_service)
+lo(rabbitmq_port)
 
-print("Establishing Pika Connection")
+lo("Establishing Pika Connection")
 
 connection = pika.BlockingConnection(
     pika.ConnectionParameters(host=rabbitmq_service, port=rabbitmq_port))
 channel = connection.channel()
 
-print("Declaring Queue")
+lo("Declaring Queue")
 
 channel.queue_declare(queue='exampleMessageQueue', durable=True) #Indicate we're working with a durable queue
 
 
-print("Defining Consumption Strategy")
+lo("Defining Consumption Strategy")
 channel.basic_consume(queue='exampleMessageQueue',
                       auto_ack=False, #Disables automatic acknowledgement (removal from queue). 
                                       #Note only one node can check out a queue item at any given time.
                       on_message_callback=callback)
 
-print(' [*] Waiting for messages. To exit press CTRL+C')
+lo(' [*] Waiting for messages. To exit press CTRL+C')
 channel.start_consuming()
