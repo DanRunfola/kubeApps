c6c443b60feb49d44bb036b2f8d8f740b99aaaf4
DanRunfola <dan@danrunfola.com>
2023-12-18 09:24:19 -0500

    Worker reconnects

--- redis/worker.py
+++ redis/worker.py
@@ -13,11 +13,35 @@ def kLog(message):
     with open("/kube/home/logs/exampleRedis/" + str(podName), "a") as f:
         f.write(str(timestamp) + " --- " + str(podName) + ":" + str(message) + "\n")
 
-# Connect to Redis
+#Connection logic
+def connect_to_redis_with_retry(redis_host, redis_port, max_retries=5, delay=2):
+    """
+    Try to connect to Redis with a specified number of retries.
+
+    :param redis_host: Hostname of the Redis server
+    :param redis_port: Port number of the Redis server
+    :param max_retries: Maximum number of connection attempts
+    :param delay: Delay between retries in seconds
+    :return: Redis connection object or None
+    """
+    retry_count = 0
+    while retry_count < max_retries:
+        try:
+            print(f"Attempt {retry_count + 1} to connect to Redis...")
+            return redis.Redis(host=redis_host, port=redis_port, db=0, decode_responses=True, socket_timeout=10)
+        except redis.exceptions.ConnectionError as e:
+            print(f"Connection failed: {e}")
+            time.sleep(delay)
+            retry_count += 1
+
+    print(f"Failed to connect to Redis after {max_retries} attempts.")
+    return None
+
 redis_host = os.getenv('REDIS_SERVICE_HOST', 'redis')
 redis_port = os.getenv('REDIS_SERVICE_PORT', '6379')
+
 # Connect to Redis
-redis_client = redis.Redis(host=redis_host, port=redis_port, db=0, decode_responses=True, socket_timeout=10)
+redis_client = connect_to_redis_with_retry(redis_host, redis_port)
 
 def process_job(redis_client, queue_name, processing_queue_name):
     job = redis_client.brpoplpush(queue_name, processing_queue_name)
