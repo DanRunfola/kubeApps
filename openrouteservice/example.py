af62b772c4c5123d74fff16a5ee2499c22153939
DanRunfola <dan@danrunfola.com>
2024-01-15 14:59:12 -0500

    curl

--- openrouteservice/example.py
+++ openrouteservice/example.py
@@ -3,6 +3,6 @@ import folium
 import polyline
 
 url = "http://osrm:80/route/v1/driving/-117.851364,33.698206;-117.838925,33.672260"
-r = requests.get(url)
+r = requests.get(url, timeout=10)
 res = r.json()
 print(res)
